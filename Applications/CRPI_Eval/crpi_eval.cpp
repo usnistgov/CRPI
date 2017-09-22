a0f59b032fdfc6161776104000a9c03089e0afce
Jeremy Marvel <jeremy.marvel@nist.gov>
2017-09-22 06:44:09 -0400

    Initial push to the repository.
    Added a couple of example programs.

---
+++ Applications/CRPI_Eval/crpi_eval.cpp
@@ -0,0 +1,456 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Original System: Collaborative Robot Programming Interface
+//  Subsystem:       CRPI Performance Evaluation
+//  Workfile:        crpi_eval.cpp
+//  Revision:        14 March, 2016
+//  Author:          J. Marvel
+//
+//  Description
+//  ===========
+//  
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <time.h>
+#include "crpi_robot.h"
+#include "crpi_kuka_lwr.h"
+#include "crpi_universal.h"
+#include "crpi_robotiq.h"
+#include "crpi_abb.h"
+#include "ulapi.h"
+#include "NumericalMath.h" 
+
+#pragma warning (disable: 4996)
+
+using namespace crpi_robot;
+using namespace std;
+
+void main ()
+{
+  int i = 0;
+  double perf1, perf2, perf3, perf4;
+  crpi_timer timer;
+  double poseadd1 = 5.0f, poseadd2 = 10.0f, jointadd1 = 0.25f, jointadd2 = 0.5f, jointadd3 = 1.0f;
+
+  //CrpiRobot<CrpiUniversal> arm("universal_ur5.xml");
+  //CrpiRobot<CrpiAbb> arm("abb_irb14000_left.xml");
+  CrpiRobot<CrpiAbb> arm("abb_irb14000_right.xml");
+  //CrpiRobot<CrpiUniversal> arm("universal_ur10_right.xml");
+  //CrpiRobot<CrpiKukaLWR> arm("kuka_lwr.xml");
+
+  arm.SetAngleUnits("degree");
+  arm.SetLengthUnits("mm");
+  arm.Couple("Yumi_Parallel");
+  robotIO curIO, tarIO;
+  robotPose curPose, origPose, tarPose1, tarPose2;
+  robotAxes curAxes, origAxes, tarAxes1, tarAxes2, tarAxes3, tarAxes4;
+
+  curPose.x = curPose.y = curPose.z = curPose.xrot = curPose.yrot = curPose.zrot = 0.0f;
+  arm.GetRobotPose(&curPose);
+  arm.GetRobotAxes(&curAxes);
+
+  cout << "Robot Pose (" << curPose.x << ", " << curPose.y << ", " << curPose.z << ", " 
+       << curPose.xrot << ", " << curPose.yrot << ", " << curPose.zrot << ")" << endl;
+  cout << "Robot Joints (" << curAxes.axis.at(0) << ", " << curAxes.axis.at(1) << ", "
+    << curAxes.axis.at(2) << ", " << curAxes.axis.at(3) << ", " << curAxes.axis.at(4) << ", "
+    << curAxes.axis.at(5) << ", " << curAxes.axis.at(6) << ")" << endl;
+  cout << "Enter 1 to run tests (robot will begin moving), 0 to quit: ";
+  cin >> i;
+
+  if (i < 1)
+  {
+    return;
+  }
+
+  origPose = curPose;
+  tarPose1 = curPose;
+  tarPose2 = curPose;
+  tarPose1.z += 10.0f;
+  tarPose2.z -= 10.0f;
+
+  origAxes = curAxes;
+  tarAxes1 = curAxes;
+  tarAxes2 = curAxes;
+  tarAxes3 = curAxes;
+  tarAxes4 = curAxes;
+  tarAxes1.axis.at(0) += 5;
+  tarAxes2.axis.at(0) -= 5;
+  tarAxes3.axis.at(0) += 10;
+  tarAxes4.axis.at(0) -= 10;
+
+  cout << "Running performance tests: " << endl;
+  cout << "  Base logic time:" << endl;
+  timer.start();
+  perf1 = 0.0f;
+  perf4 = 0.0f;
+  for (i = 0; i < 100; ++i)
+  {
+    if (poseadd1 < poseadd2)
+    {
+      perf4 *= perf1;
+    }
+    else
+    {
+      perf4 += perf1;
+    }
+  }
+  perf1 = timer.stop();
+  cout << "    Total time: " << perf1 << endl;
+  perf1 = perf1 / 100.0f;
+  perf3 = 1000.0f / perf1;
+  cout << "    Logic sample: Avg = " << perf1 << "ms, " << perf3 << "Hz" << endl;
+
+  cout << "  Feedback test:" << endl;
+  timer.start();
+  perf4 = 0.0f;
+  for (i = 0; i < 100; ++i)
+  {
+    if (arm.GetRobotPose(&curPose) != CANON_SUCCESS)
+    {
+      perf4++;
+    }
+  }
+  perf1 = timer.stop();
+  cout << "    Total time: " << perf1 << endl;
+  perf1 = perf1 / 100.0f;
+  perf3 = 1000.0f / perf1;
+  cout << "    Pose feedback: Avg = " << perf1 << "ms, " << perf3 << "Hz, " << perf4 << " misses" << endl;
+
+  perf4 = 0.0f;
+  timer.restart();
+  for (i = 0; i < 100; ++i)
+  {
+    if (arm.GetRobotAxes(&curAxes) != CANON_SUCCESS)
+    {
+      perf4++;
+    }
+  }
+  perf1 = timer.stop();
+  cout << "    Total time: " << perf1 << endl;
+
+  perf1 = perf1 / 100.0f;
+  perf3 = 1000.0f / perf1;
+  cout << "    Joint feedback: Avg = " << perf1 << "ms, " << perf3 << "Hz, " << perf4 << " misses" << endl;
+
+  timer.restart();
+  perf4 = 0.0f;
+  for (i = 0; i < 100; ++i)
+  {
+    if (arm.GetRobotIO(&curIO) != CANON_SUCCESS)
+    {
+      perf4++;
+    }
+  }
+  perf1 = timer.stop();
+  cout << "    Total time:  " << perf1 << endl;
+  perf1 = perf1 / 100.0f;
+  perf3 = 1000.0f / perf1;
+  cout << "    IO feedback: Avg = " << perf1 << "ms, " << perf3 << "Hz, " << perf4 << " misses" << endl;
+
+
+  cout << "  Motion test:" << endl;
+  cout << "    Zero motion:" << endl;
+  timer.restart();
+  perf1 = 0.0f;
+  perf2 = 0.0f;
+  for (i = 0; i < 100; ++i)
+  {
+    if (arm.MoveStraightTo(origPose) != CANON_SUCCESS)
+    {
+      ++perf2;
+    }
+    else
+    {
+      arm.GetRobotPose(&curPose);
+      perf1 += curPose.distance(origPose);
+    }
+  }
+  perf4 = timer.stop();
+  cout << "      Total Time:  " << perf4 << endl;
+  perf1 = perf1 / 100.0f;
+  perf4 = perf4 / 100.0f;
+  perf3 = 1000.0f / perf4;
+  cout << "      LIN:  Avg Error = " << perf1 << "mm, " << "Avg = " << perf4 << "ms, " << perf3 << "Hz, " << perf2 << " misses " << endl;
+
+  timer.restart();
+  perf1 = 0.0f;
+  perf2 = 0.0f;
+  for (i = 0; i < 100; ++i)
+  {
+    if (arm.MoveTo(origPose) != CANON_SUCCESS)
+    {
+      ++perf2;
+    }
+    else
+    {
+      arm.GetRobotPose(&curPose);
+      perf1 += curPose.distance(origPose);
+    }
+  }
+  perf4 = timer.stop();
+  cout << "      Total Time:  " << perf4 << endl;
+  perf1 = perf1 / 100.0f;
+  perf4 = perf4 / 100.0f;
+  perf3 = 1000.0f / perf4;
+  cout << "      PTP:  Avg Error = " << perf1 << "mm, " << "Avg = " << perf4 << "ms, " << perf3 << "Hz, " << perf2 << " misses " << endl;
+
+  timer.restart();
+  perf1 = 0.0f;
+  perf2 = 0.0f;
+  for (i = 0; i < 100; ++i)
+  {
+    if (arm.MoveToAxisTarget(origAxes) != CANON_SUCCESS)
+    {
+      ++perf2;
+    }
+    else
+    {
+      arm.GetRobotAxes(&curAxes);
+      //cout << "------" << endl;
+      //curAxes.print();
+      //origAxes.print();
+      perf1 += curAxes.error(origAxes);
+      //cout << perf1 << endl;
+      //cout << "------" << endl;
+    }
+  }
+  perf4 = timer.stop();
+  cout << "      Total Time:  " << perf4 << endl;
+  perf1 = perf1 / 100.0f;
+  perf4 = perf4 / 100.0f;
+  perf3 = 1000.0f / perf4;
+  cout << "      Joint:  Avg Error = " << perf1 << "deg, " << "Avg = " << perf4 << "ms, " << perf3 << "Hz, " << perf2 << " misses " << endl;
+
+  cout << "    Oscillation:" << endl;
+
+  arm.MoveStraightTo(origPose);
+
+  tarPose1 = origPose;
+  tarPose2 = origPose;
+  tarPose1.z += poseadd1;
+  tarPose2.z -= poseadd1;
+  cout << "       LIN:  Orig +/- " << poseadd1 << "mm" << endl;
+
+  timer.restart();
+  perf1 = 0.0f;
+  perf2 = 0.0f;
+  perf3 = 0.0f;
+  for (i = 0; i < 50; ++i)
+  {
+    if (arm.MoveStraightTo(tarPose1) == CANON_SUCCESS)
+    {
+      arm.GetRobotPose(&curPose);
+      perf1 += tarPose1.distance(curPose);
+    }
+    else
+    {
+      ++perf3;
+    }
+
+    if (arm.MoveStraightTo(tarPose2) == CANON_SUCCESS)
+    {
+      arm.GetRobotPose(&curPose);
+      perf2 += tarPose2.distance(curPose);
+    }
+    else
+    {
+      ++perf3;
+    }
+  }
+  perf4 = timer.stop();
+  cout << "         Total Time:  " << perf4 << endl;
+  perf1 = perf1 / 50.0f;
+  perf2 = perf2 / 50.0f;
+  perf4 = perf4 / 100.0f;
+  cout << "         LIN:  Avg Err = (" << perf1 << ", " << perf2 << ")mm, " << perf4 << "ms, ";
+  perf4 = 1000.0f / perf4;
+  cout << perf4 << "Hz, " << perf3 << " misses" << endl;
+
+
+  arm.MoveStraightTo(origPose);
+
+  tarPose1 = origPose;
+  tarPose2 = origPose;
+  tarPose1.z += poseadd2;
+  tarPose2.z -= poseadd2;
+  cout << "       LIN:  Orig +/- " << poseadd2 << "mm" << endl;
+
+  timer.restart();
+  perf1 = 0.0f;
+  perf2 = 0.0f;
+  perf3 = 0.0f;
+  for (i = 0; i < 50; ++i)
+  {
+    if (arm.MoveStraightTo(tarPose1) == CANON_SUCCESS)
+    {
+      arm.GetRobotPose(&curPose);
+      perf1 += tarPose1.distance(curPose);
+    }
+    else
+    {
+      ++perf3;
+    }
+
+    if (arm.MoveStraightTo(tarPose2) == CANON_SUCCESS)
+    {
+      arm.GetRobotPose(&curPose);
+      perf2 += tarPose2.distance(curPose);
+    }
+    else
+    {
+      ++perf3;
+    }
+  }
+  perf4 = timer.stop();
+  cout << "         Total Time:  " << perf4 << endl;
+  perf1 = perf1 / 50.0f;
+  perf2 = perf2 / 50.0f;
+  perf4 = perf4 / 100.0f;
+  cout << "         LIN:  Avg Err = (" << perf1 << ", " << perf2 << ")mm, " << perf4 << "ms, ";
+  perf4 = 1000.0f / perf4;
+  cout << perf4 << "Hz, " << perf3 << " misses" << endl;
+
+
+
+  arm.MoveToAxisTarget(origAxes);
+
+  tarAxes1 = origAxes;
+  tarAxes2 = origAxes;
+  tarAxes1.axis.at(0) += jointadd1;
+  tarAxes2.axis.at(0) -= jointadd1;
+  cout << "       Joint:  Orig +/- " << jointadd1 << "deg" << endl;
+
+  timer.restart();
+  perf1 = 0.0f;
+  perf2 = 0.0f;
+  perf3 = 0.0f;
+  for (i = 0; i < 50; ++i)
+  {
+    if (arm.MoveToAxisTarget(tarAxes1) == CANON_SUCCESS)
+    {
+      arm.GetRobotAxes(&curAxes);
+      perf1 += tarAxes1.error(curAxes);
+    }
+    else
+    {
+      ++perf3;
+    }
+      
+    if (arm.MoveToAxisTarget(tarAxes2) == CANON_SUCCESS)
+    {
+      arm.GetRobotAxes(&curAxes);
+      perf2 += tarAxes2.error(curAxes);
+    }
+    else
+    {
+      ++perf3;
+    }
+  }
+  perf4 = timer.stop();
+  cout << "         Total Time:  " << perf4 << endl;
+  perf1 = perf1 / 50.0f;
+  perf2 = perf2 / 50.0f;
+  perf4 = perf4 / 100.0f;
+  cout << "         Joint:  Avg Err = (" << perf1 << ", " << perf2 << ")deg, " << perf4 << "ms, ";
+  perf4 = 1000.0f / perf4;
+  cout << perf4 << "Hz, " << perf3 << " misses" << endl;
+
+
+  arm.MoveToAxisTarget(origAxes);
+
+  tarAxes1 = origAxes;
+  tarAxes2 = origAxes;
+  tarAxes1.axis.at(0) += jointadd2;
+  tarAxes2.axis.at(0) -= jointadd2;
+  cout << "       Joint:  Orig +/- " << jointadd2 << "deg" << endl;
+
+  timer.restart();
+  perf1 = 0.0f;
+  perf2 = 0.0f;
+  perf3 = 0.0f;
+  for (i = 0; i < 50; ++i)
+  {
+    if (arm.MoveToAxisTarget(tarAxes1) == CANON_SUCCESS)
+    {
+      arm.GetRobotAxes(&curAxes);
+      perf1 += tarAxes1.error(curAxes);
+    }
+    else
+    {
+      ++perf3;
+    }
+
+    if (arm.MoveToAxisTarget(tarAxes2) == CANON_SUCCESS)
+    {
+      arm.GetRobotAxes(&curAxes);
+      perf2 += tarAxes2.error(curAxes);
+    }
+    else
+    {
+      ++perf3;
+    }
+  }
+  perf4 = timer.stop();
+  cout << "         Total Time:  " << perf4 << endl;
+  perf1 = perf1 / 50.0f;
+  perf2 = perf2 / 50.0f;
+  perf4 = perf4 / 100.0f;
+  cout << "         Joint:  Avg Err = (" << perf1 << ", " << perf2 << ")deg, " << perf4 << "ms, ";
+  perf4 = 1000.0f / perf4;
+  cout << perf4 << "Hz, " << perf3 << " misses" << endl;
+
+
+  arm.MoveToAxisTarget(origAxes);
+
+  tarAxes1 = origAxes;
+  tarAxes2 = origAxes;
+  tarAxes1.axis.at(0) += jointadd3;
+  tarAxes2.axis.at(0) -= jointadd3;
+  cout << "       Joint:  Orig +/- " << jointadd3 << "deg" << endl;
+
+  timer.restart();
+  perf1 = 0.0f;
+  perf2 = 0.0f;
+  perf3 = 0.0f;
+  for (i = 0; i < 50; ++i)
+  {
+    if (arm.MoveToAxisTarget(tarAxes1) == CANON_SUCCESS)
+    {
+      arm.GetRobotAxes(&curAxes);
+      perf1 += tarAxes1.error(curAxes);
+    }
+    else
+    {
+      ++perf3;
+    }
+
+    if (arm.MoveToAxisTarget(tarAxes2) == CANON_SUCCESS)
+    {
+      arm.GetRobotAxes(&curAxes);
+      perf2 += tarAxes2.error(curAxes);
+    }
+    else
+    {
+      ++perf3;
+    }
+  }
+  perf4 = timer.stop();
+  cout << "         Total Time:  " << perf4 << endl;
+  perf1 = perf1 / 50.0f;
+  perf2 = perf2 / 50.0f;
+  perf4 = perf4 / 100.0f;
+  cout << "         Joint:  Avg Err = (" << perf1 << ", " << perf2 << ")deg, " << perf4 << "ms, ";
+  perf4 = 1000.0f / perf4;
+  cout << perf4 << "Hz, " << perf3 << " misses" << endl;
+
+  arm.MoveToAxisTarget(origAxes);
+
+  cout << "All done" << endl;
+
+}
+
+
---
+++ Applications/XMLInterface/XMLInterface.cpp
@@ -0,0 +1,434 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Original System: Collaborative Robot Programming Interface
+//  Subsystem:       CRPI XML Handler
+//  Workfile:        XMLInterface.cpp
+//  Revision:        1.0 - 20 September, 2016
+//
+//  Description
+//  ===========
+//  CRPI XML handler application.  Creates threads for each robot specified
+//  in settings.dat, each of which opens a server on a specified port that
+//  takes XML commands from a remote client.
+///////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <fstream>
+#include "crpi_robot.h"
+#include "crpi_abb.h"
+#include "crpi_kuka_lwr.h"
+#include "crpi_universal.h"
+#include "crpi_robotiq.h"
+#include "ulapi.h"
+
+//#define XMLINTERFACE_NOISY
+#define XMLINTERFACE_DEBUGTEST
+
+using namespace std;
+using namespace crpi_robot;
+
+typedef CrpiUniversal robArmType;
+
+
+struct globalHandle
+{
+  //! @brief Whether or not to continue running this thread
+  //!
+  bool runThread;  
+  
+  //! @brief Path to the robot's CRPI configuration XML file
+  string path;
+
+  //! @brief Port to open for client connection
+  //!
+  int port;
+
+  //! @brief Constructor
+  //!
+  //! @param armptr Pointer to the robot object (for getting feedback from the robot)
+  //!
+  globalHandle ()
+  {
+    runThread = false;
+    path = string();
+  }
+
+  //! @brief Destructor
+  //!
+  ~globalHandle ()
+  {
+  }
+};
+
+
+//! @brief Thread method for communicating with an ABB robot
+//!
+//! @param param Pointer to a globalHandle object containing runtime instructions
+//!
+void armABBHandlerThread(void *param)
+{
+  globalHandle *gH = (globalHandle*)param;
+  CrpiRobot<CrpiAbb> arm(gH->path.c_str());
+  ulapi_integer server, client;
+  bool clientConnected = false;
+
+  crpi_timer timer;
+  char buffer[2048];
+  string str;
+  ulapi_integer rec, sent;
+
+  //! Create socket connection
+  server = ulapi_socket_get_server_id(gH->port);
+  ulapi_socket_set_blocking(server);
+
+  while (gH->runThread)
+  {
+    if (!clientConnected)
+    {
+      cout << "Running XML Interface on port " << gH->port << " for the ABB arm" << endl;
+      client = ulapi_socket_get_connection_id(server);
+      ulapi_socket_set_blocking(client);
+      clientConnected = true;
+      cout << "Remote ABB client connected..." << endl;
+    }
+
+    while (clientConnected && gH->runThread)
+    {
+      rec = ulapi_socket_read(client, buffer, 2048);
+      if (rec > 0)
+      {
+        str = buffer;
+        arm.CrpiXmlHandler(str);
+        arm.CrpiXmlResponse(buffer);
+        sent = ulapi_socket_write(client, buffer, strlen(buffer));
+      }
+      else
+      {
+        timer.waitUntil(5);
+      }
+    } // while (clientConnected && gH->runThread)
+  } // while (gH->runThread)
+
+  gH = NULL;
+  return;
+}
+
+
+//! @brief Thread method for communicating with a UR robot
+//!
+//! @param param Pointer to a globalHandle object containing runtime instructions
+//!
+void armUniversalHandlerThread(void *param)
+{
+  globalHandle *gH = (globalHandle*)param;
+  cout << "Creating robot using " << gH->path.c_str() << endl;
+  CrpiRobot<CrpiUniversal> arm(gH->path.c_str());
+  cout << "Robot Created" << endl;
+  ulapi_integer server, client;
+  bool clientConnected = false;
+
+  crpi_timer timer;
+  char buffer[2048];
+  string str;
+  ulapi_integer rec, sent;
+
+  //! Create socket connection
+  server = ulapi_socket_get_server_id(gH->port);
+  ulapi_socket_set_blocking(server);
+
+  while (gH->runThread)
+  {
+    if (!clientConnected)
+    {
+      cout << "Running XML Interface on port " << gH->port << " for the Universal arm" << endl;
+      client = ulapi_socket_get_connection_id(server);
+      ulapi_socket_set_blocking(client);
+      clientConnected = true;
+      cout << "Remote Universal client connected..." << endl;
+    }
+
+    while (clientConnected && gH->runThread)
+    {
+      rec = ulapi_socket_read(client, buffer, 2048);
+      if (rec > 0)
+      {
+        str = buffer;
+        arm.CrpiXmlHandler(str);
+
+        arm.CrpiXmlResponse(buffer);
+        sent = ulapi_socket_write(client, buffer, strlen(buffer));
+      }
+      else
+      {
+        timer.waitUntil(5);
+      }
+    } // while (clientConnected && gH->runThread)
+  } // while (gH->runThread)
+
+  gH = NULL;
+  return;
+}
+
+
+//! @brief Thread method for communicating with a Kuka robot
+//!
+//! @param param Pointer to a globalHandle object containing runtime instructions
+//!
+void armKukaHandlerThread(void *param)
+{
+  globalHandle *gH = (globalHandle*)param;
+  CrpiRobot<CrpiKukaLWR> arm(gH->path.c_str());
+  ulapi_integer server, client;
+  bool clientConnected = false;
+
+  crpi_timer timer;
+  char buffer[2048];
+  string str;
+  ulapi_integer rec, sent;
+
+  //! Create socket connection
+  server = ulapi_socket_get_server_id(gH->port);
+  ulapi_socket_set_blocking(server);
+
+  while (gH->runThread)
+  {
+    if (!clientConnected)
+    {
+      cout << "Running XML Interface on port " << gH->port << " for the KUKA arm" << endl;
+      client = ulapi_socket_get_connection_id(server);
+      ulapi_socket_set_blocking(client);
+      clientConnected = true;
+      cout << "Remote KUKA client connected..." << endl;
+    }
+
+    while (clientConnected && gH->runThread)
+    {
+      rec = ulapi_socket_read(client, buffer, 2048);
+      if (rec > 0)
+      {
+        str = buffer;
+        arm.CrpiXmlHandler(str);
+        arm.CrpiXmlResponse(buffer);
+        sent = ulapi_socket_write(client, buffer, strlen(buffer));
+      }
+      else
+      {
+        timer.waitUntil(5);
+      }
+    } // while (clientConnected && gH->runThread)
+  } // while (gH->runThread)
+
+  gH = NULL;
+  return;
+}
+
+
+//! @brief Thread method for communicating with a Robotiq robot hand
+//!
+//! @param param Pointer to a globalHandle object containing runtime instructions
+//!
+void armRobotiqHandlerThread(void *param)
+{
+  globalHandle *gH = (globalHandle*)param;
+  CrpiRobot<CrpiRobotiq> arm(gH->path.c_str());
+  ulapi_integer server, client;
+  bool clientConnected = false;
+
+  crpi_timer timer;
+  char buffer[2048];
+  string str;
+  ulapi_integer rec, sent;
+
+  //! Create socket connection
+  server = ulapi_socket_get_server_id(gH->port);
+  ulapi_socket_set_blocking(server);
+
+  while (gH->runThread)
+  {
+    if (!clientConnected)
+    {
+      cout << "Running XML Interface on port " << gH->port << " for the Robotiq arm" << endl;
+      client = ulapi_socket_get_connection_id(server);
+      ulapi_socket_set_blocking(client);
+      clientConnected = true;
+      cout << "Remote RObotiq client connected..." << endl;
+    }
+
+    while (clientConnected && gH->runThread)
+    {
+      rec = ulapi_socket_read(client, buffer, 2048);
+      if (rec > 0)
+      {
+        str = buffer;
+        arm.CrpiXmlHandler(str);
+        arm.CrpiXmlResponse(buffer);
+        sent = ulapi_socket_write(client, buffer, strlen(buffer));
+      }
+      else
+      {
+        timer.waitUntil(5);
+      }
+    } // while (clientConnected && gH->runThread)
+  } // while (gH->runThread)
+
+  gH = NULL;
+  return;
+}
+
+
+//! @brief Example client thread that sends simple up/down commands and gets feedback
+//!
+//! @param param Pointer to a globalHandle object containing runtime instructions
+//!
+void nodDemo(void *param)
+{
+  globalHandle *gH = (globalHandle*)param;
+  ulapi_integer server, got;
+  char inbuffer[REQUEST_MSG_SIZE];
+
+  server = ulapi_socket_get_client_id(30012, "127.0.0.1");
+  if (server < 0)
+  {
+    cout << "could not connect sample client" << endl;
+    return;
+  }
+  else
+  {
+    cout << "connected sample client" << endl;
+  }
+
+  sprintf(inbuffer, "<CRPICommand type=\"Couple\"><String Value = \"flange_ring\"/></CRPICommand>");
+  ulapi_socket_write(server, inbuffer, strlen(inbuffer));
+  Sleep(1000);
+
+  bool on = true;
+  while (true)
+  {
+    sprintf(inbuffer, "<CRPICommand type=\"SetRobotDO\"><Int Value = \"0\">/<Boolean Value = \"%s\"/></CRPICommand>", (on ? "true" : "false"));
+    //cout << "sending " << inbuffer << endl;
+    ulapi_socket_write(server, inbuffer, strlen(inbuffer));
+
+    got = ulapi_socket_read(server, inbuffer, REQUEST_MSG_SIZE);
+    inbuffer[got] = '\0';
+    cout << got << " " << inbuffer << endl;
+    Sleep(1000);
+    on = !on;
+  } // while (gH->runThread)
+
+  gH = NULL;
+  return;
+}
+
+
+//! @brief Main program method
+//!
+void main ()
+{
+  ifstream infile("xmlsettings.dat");
+  string robot, path;
+  int port;
+  vector<globalHandle> handles;
+  vector<void*> armTasks;
+  void *armtask;
+  globalHandle handle;
+
+#ifdef XMLINTERFACE_DEBUGTEST
+  globalHandle demoHandle;
+  void *demoTask;
+#endif
+
+
+  while (infile >> robot)
+  {
+    infile >> path >> port;
+
+    //! Start the threads based on the information parsed from settings.dat
+    cout << "Starting server for " << robot << " on port " << port << endl;
+    if (robot == "ABB_IRB_14000")
+    {
+      handle.runThread = true;
+      handle.path = path;
+      handle.port = port;
+
+      armtask = ulapi_task_new();
+      //! Start new ABB thread
+      ulapi_task_start((ulapi_task_struct*)armtask, armABBHandlerThread, &handle, ulapi_prio_lowest(), 0);
+      handles.push_back(handle);
+      armTasks.push_back(armtask);
+    }
+    else if (robot == "UNIVERSAL")
+    {
+      handle.runThread = true;
+      handle.path = path;
+      handle.port = port;
+
+      armtask = ulapi_task_new();
+      //! Start new Universal Robot thread
+      ulapi_task_start((ulapi_task_struct*)armtask, armUniversalHandlerThread, &handle, ulapi_prio_lowest(), 0);
+      handles.push_back(handle);
+      armTasks.push_back(armtask);
+    }
+    else if (robot == "KUKA_LWR")
+    {
+      handle.runThread = true;
+      handle.path = path;
+      handle.port = port;
+
+      armtask = ulapi_task_new();
+      //! Start new Kuka thread
+      ulapi_task_start((ulapi_task_struct*)armtask, armKukaHandlerThread, &handle, ulapi_prio_lowest(), 0);
+      handles.push_back(handle);
+      armTasks.push_back(armtask);
+    }
+    else if (robot == "ROBOTIQ")
+    {
+      handle.runThread = true;
+      handle.path = path;
+      handle.port = port;
+
+      armtask = ulapi_task_new();
+      //! Start new Kuka thread
+      ulapi_task_start((ulapi_task_struct*)armtask, armRobotiqHandlerThread, &handle, ulapi_prio_lowest(), 0);
+      handles.push_back(handle);
+      armTasks.push_back(armtask);
+    }
+    else
+    {
+      cout << "Error in xmlsettings.dat.  Unknown robot type: " << robot << endl;
+    }
+    Sleep(1000);
+  } // while (infile >> robot)
+
+
+#ifdef XMLINTERFACE_DEBUGTEST
+  Sleep(5000);
+  demoTask = ulapi_task_new();
+  ulapi_task_start((ulapi_task_struct*)demoTask, nodDemo, &demoHandle, ulapi_prio_lowest(), 0);
+
+#endif
+
+
+  crpi_timer timer;
+  while (true)
+  {
+    //! Put any additional logic here
+    
+    //! Throttle back evaluation of the main thread to keep from starving the other threads
+    timer.waitUntil(500);
+  } // while (true)
+
+  vector<globalHandle>::iterator ghIter;
+  vector<void*>::iterator taskIter;
+
+  //! Garbage collection.  Stop the threads.
+  for (ghIter = handles.begin(), taskIter = armTasks.begin(); ghIter != handles.end(); ++ghIter, ++taskIter)
+  {
+    ghIter->runThread = false;
+    timer.waitUntil(100);
+    ulapi_task_stop((ulapi_task_struct*)*taskIter);
+  }
+}
